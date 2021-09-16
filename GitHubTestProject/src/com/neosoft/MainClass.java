e194b58faaa7e0d85495f1e212de7431bfb61d67
pradnyeshgaonkar123 <pradnyeshgaonkar106@gmail.com>
2021-09-16 14:07:18 +0530

    Initial commit by TL

--- GitHubTestProject/src/com/neosoft/MainClass.java
+++ GitHubTestProject/src/com/neosoft/MainClass.java
@@ -2,6 +2,6 @@ package com.neosoft;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("Write your code here member");
+		System.out.println("Members, You can write your code here..");
 	}
 }
